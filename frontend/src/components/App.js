c6723e664b2fb6766fd8e55718b5d4face25b792
aleksei.stsigartsov <aleksei.stsigartsov@gmail.com>
2022-07-17 19:15:43 +0300

    frontend creation

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react';
+
+
+class App extends Component {
+    render() {
+        return <h1>WE ARE CONNECTED!!!!</h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
